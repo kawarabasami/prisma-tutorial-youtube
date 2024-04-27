7685347910d80c4e85bbc0199dc4a17353d41ce2
m-nakamine <kawarabasami.dev@gmail.com>
2024-04-27 12:46:04 +0900

    add post method

--- server.ts
+++ server.ts
@@ -1,7 +1,23 @@
+import { PrismaClient } from '@prisma/client';
 import express from 'express';
 
 const app = express();
 const PORT = 8000;
+
+app.use(express.json());
+
+const prisma = new PrismaClient();
+
+app.post('/', async (req, res) => {
+  const { title, body } = req.body;
+  const posts = await prisma.posts.create({
+    data: {
+      title,
+      body
+    }
+  });
+  return res.json(posts);
+});
 
 app.listen(PORT, () => {
   console.log('サーバが起動中...');
